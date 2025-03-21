fbc9566cfc15ee40b74637edbcf9cf54f1d7b855
Mert Hacioglu <mhacioglu96@gmail.com>
2025-03-21 16:15:43 +0000

    Property record, data loader, profile implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
 }
 
