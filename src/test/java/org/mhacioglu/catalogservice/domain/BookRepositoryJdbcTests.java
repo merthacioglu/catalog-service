a17f991c6d80053bd1c406bfe6fb453b80dc852e
Mert Hacioglu <mhacioglu96@gmail.com>
2025-04-06 22:34:29 +0100

    New tests added for json serialisation and repository methods

--- src/test/java/org/mhacioglu/catalogservice/domain/BookRepositoryJdbcTests.java
+++ src/test/java/org/mhacioglu/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -9,7 +9,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Instant;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,18 +28,72 @@ public class BookRepositoryJdbcTests {
     private JdbcAggregateTemplate jdbcAggregateTemplate;
 
     @Test
+    void findAllBooks() {
+        var book1 = new Book(null, Instant.now(), Instant.now(),
+                "1231231234", "Test title 1", "Test author 1",
+                22.90, "Test publisher", 0);
+        var book2 = new Book(null, Instant.now(), Instant.now(),
+                "0987654321", "Test title 2", "Test author 2",
+                16.90, "Test publisher", 0);
+        jdbcAggregateTemplate.insert(book1);
+        jdbcAggregateTemplate.insert(book2);
+
+        Iterable<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2.isbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
+
+    @Test
     void findBookByIsbnWhenExisting() {
-        var bookIsbn = "1234561235";
-        var book = new Book(null, null, null,
-                bookIsbn, "Title", "Author", 12.90, null, 0);
-        var expectedBook = jdbcAggregateTemplate.insert(book);
+        String isbn = "1231231234";
+        var book = new Book(null, Instant.now(), Instant.now(),
+                isbn, "Test title 1", "Test author 1",
+                22.90, "Test publisher", 0);
+        jdbcAggregateTemplate.insert(book);
 
-        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+        Optional<Book> actualBook = bookRepository.findByIsbn(isbn);
+
         assertThat(actualBook).isPresent();
-        assertThat(actualBook.get())
-                .usingRecursiveComparison()
-                .ignoringFields("createdDate", "lastModifiedDate")
-                .isEqualTo(expectedBook);
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
 
+    @Test
+    void findBookByIsbnWhenNotExisting() {
+        Optional<Book> actualBook = bookRepository.findByIsbn("1234561238");
+        assertThat(actualBook).isEmpty();
+    }
+
+    @Test
+    void existsByIsbnWhenExisting() {
+        String isbn = "1234561239";
+        var book = new Book(null, Instant.now(), Instant.now(),
+                isbn, "Test title 1", "Test author 1",
+                22.90, "Test publisher", 0);
+        jdbcAggregateTemplate.insert(book);
+
+        boolean existing = bookRepository.existsByIsbn(isbn);
+
+        assertThat(existing).isTrue();
+    }
+
+    @Test
+    void existsByIsbnWhenNotExisting() {
+        boolean existing = bookRepository.existsByIsbn("1234561240");
+        assertThat(existing).isFalse();
+    }
+
+    @Test
+    void deleteByIsbn() {
+        var isbn = "1234561241";
+        var book = new Book(null, Instant.now(), Instant.now(),
+                isbn, "Test title 1", "Test author 1",
+                22.90, "Test publisher", 0);
+        var persistedBook = jdbcAggregateTemplate.insert(book);
+
+        bookRepository.deleteByIsbn(persistedBook.isbn());
+
+        assertThat(jdbcAggregateTemplate.findById(persistedBook.id(), Book.class)).isNull();
     }
 }
--- src/test/java/org/mhacioglu/catalogservice/web/BookJsonTests.java
+++ src/test/java/org/mhacioglu/catalogservice/web/BookJsonTests.java
@@ -18,9 +18,13 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = Book.build("1234567890", Instant.now(), Instant.now(),
-                "Title", "Author", 9.90, null);
+        var book = new Book(212L,  Instant.now(), Instant.now(),
+                "1234567890", "Test title", "Test author", 15.50,
+                "Test publisher", 5);
+
         var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(book.id());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title")
@@ -29,22 +33,36 @@ public class BookJsonTests {
                 .isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.publisher")
+                .isEqualTo(book.publisher());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdDate")
+                .isEqualTo(book.createdDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate")
+                .isEqualTo(book.lastModifiedDate().toString());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
+                .isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
+        var instant = Instant.parse("2023-10-01T12:00:00Z");
         var content = """
                 {
+                "id": 212,
+                "createdDate": "2023-10-01T12:00:00Z",
+                "lastModifiedDate": "2023-10-01T12:00:00Z",
                 "isbn": "1234567890",
                 "title": "Title",
                 "author": "Author",
-                "price": 9.90
+                "price": 9.90,
+                "publisher": "Polarsophia",
+                "version": 5
                 }
                 """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(Book.build("1234567890", null, null,
-                        "Title", "Author", 9.90, null));
+                .isEqualTo(new Book(212L, instant, instant, "1234567890",
+                        "Title", "Author", 9.90, "Polarsophia", 5));
     }
 
 }
